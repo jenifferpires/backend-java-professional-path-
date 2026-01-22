bb2e3fdc03dc69ee4cadffe8d634c3dd7e29535e
Jeniffer Pires <jenyfferhellen@gmail.com>
2026-01-22 00:21:07 -0300

    feat: add primitive types example in Java

---
+++ 01-fundamentos-java/exemplos/TiposPrimitivos.java
@@ -0,0 +1,28 @@
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+
+        // Tipos numéricos inteiros
+        int idade = 30;
+        long populacaoMundial = 8000000000L;
+
+        // Tipos numéricos decimais
+        double altura = 1.65;
+        float peso = 60.5f;
+
+        // Tipo lógico
+        boolean javaEhLegal = true;
+
+        // Tipo caractere
+        char inicial = 'J';
+
+        // Exibindo os valores no console
+        System.out.println("Idade: " + idade);
+        System.out.println("População mundial: " + populacaoMundial);
+        System.out.println("Altura: " + altura);
+        System.out.println("Peso: " + peso);
+        System.out.println("Java é legal? " + javaEhLegal);
+        System.out.println("Inicial: " + inicial);
+    }
+
+}
